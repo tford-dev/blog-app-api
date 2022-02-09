816f0e67c49d4f19913ec03d3dc436a00624f999
tford-dev <terranceford513@gmail.com>
2022-02-09 17:48:20 -0500

    Routes are all functioning

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/category");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -15,8 +18,24 @@ mongoose.connect(process.env.MONGO_URL, {
     .then(console.log("Database is connected"))
 	.catch((err)=> console.log(err));
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images")
+    }, filename: (req, file, cb) => {
+        cb(null, req.body.name);
+        // cb(null, "image.jpg");
+    }
+});
+
+const upload = multer({storage:storage});
+app.post("/api/upload", upload.single("file"), (req, res)=> {
+    res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", ()=> {
     console.log("Backend is connected");
--- models/Post.js
+++ models/Post.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema(
     {
+        username: {
+            type: String,
+            required: true
+        },
         title: {
             type: String,
             required: true,
-            unique: true
         },
         description: {
             type: String,
@@ -13,7 +16,7 @@ const PostSchema = new mongoose.Schema(
         },
         photo: {
             type: String,
-            required: true,
+            required: false,
         },
         categories: {
             type: Array,
